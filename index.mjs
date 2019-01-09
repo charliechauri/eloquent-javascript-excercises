a8b91943f9213857f01c19065b5b10f873d25500
charliechauri <carlos.echauri@gmail.com>
2019-01-09 00:39:18 -0600

    Add entry point for running exercises

---
+++ index.mjs
@@ -0,0 +1,27 @@
+/**
+* @name index
+* @description This script runs all exercises
+* @todo Add command line parameters 'chapter' and 'exercise' to select a specific exercise to run.
+*/
+
+import displayChessBoard from './chapter-2-program-structure/chess-board';
+import displayTriangle from './chapter-2-program-structure/triangle';
+import fizzBuzz from './chapter-2-program-structure/fizz-buzz';
+import min from './chapter-3-functions/minimum';
+import isEven from './chapter-3-functions/recursion';
+import { countBs } from './chapter-3-functions/bean-counting';
+
+// Chapter 2
+displayTriangle();
+displayChessBoard(7, '$');
+fizzBuzz();
+
+// Chapter 3
+console.log(`Minimum number of 10 and 2 is: ${min(10, 2)}`);
+console.log(`Is ${0} even? ${isEven(0)}`);
+console.log(`Is ${1} even? ${isEven(1)}`);
+console.log(`Is ${5} even? ${isEven(5)}`);
+console.log(`Is ${-10} even? ${isEven(-10)}`);
+console.log(`Number of 'B' included on 'BB-8': ${countBs()}`);
+console.log(`Number of 'B' included on 'Hello': ${countBs('Hello')}`);
+console.log(`Number of 'B' included on 'Bebe': ${countBs('Bebe')}`);
