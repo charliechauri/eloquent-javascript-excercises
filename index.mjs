c3819f1ec9441169832e3e4ce3d7eadc080105f7
charliechauri <carlos.echauri@gmail.com>
2019-01-10 15:34:04 -0600

    feat Add exercise dictionary by chapter and number of exercise

--- index.mjs
+++ index.mjs
@@ -11,17 +11,28 @@ import min from './exercises/chapter-3-functions/minimum';
 import isEven from './exercises/chapter-3-functions/recursion';
 import { countBs } from './exercises/chapter-3-functions/bean-counting';
 
-// Chapter 2
-displayTriangle();
-displayChessBoard(7, '$');
-fizzBuzz();
-
-// Chapter 3
-console.log(`Minimum number of 10 and 2 is: ${min(10, 2)}`);
-console.log(`Is ${0} even? ${isEven(0)}`);
-console.log(`Is ${1} even? ${isEven(1)}`);
-console.log(`Is ${5} even? ${isEven(5)}`);
-console.log(`Is ${-10} even? ${isEven(-10)}`);
-console.log(`Number of 'B' included on 'BB-8': ${countBs()}`);
-console.log(`Number of 'B' included on 'Hello': ${countBs('Hello')}`);
-console.log(`Number of 'B' included on 'Bebe': ${countBs('Bebe')}`);
+const exercises = {
+  chapters: {
+    2: {
+      1: () => displayTriangle(),
+      2: () => displayChessBoard(7, '$'),
+      3: () => fizzBuzz(),
+    },
+    3: {
+      1: () => {
+        console.log(`Minimum number of 10 and 2 is: ${min(10, 2)}`);
+      },
+      2: () => {
+        console.log(`Is ${0} even? ${isEven(0)}`);
+        console.log(`Is ${1} even? ${isEven(1)}`);
+        console.log(`Is ${5} even? ${isEven(5)}`);
+        console.log(`Is ${-10} even? ${isEven(-10)}`);
+      },
+      3: () => {
+        console.log(`Number of 'B' included on 'BB-8': ${countBs()}`);
+        console.log(`Number of 'B' included on 'Hello': ${countBs('Hello')}`);
+        console.log(`Number of 'B' included on 'Bebe': ${countBs('Bebe')}`);
+      },
+    },
+  },
+};
